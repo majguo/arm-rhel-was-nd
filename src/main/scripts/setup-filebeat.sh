0752b59080cb6f2b1a7ea05b4ecd04eb6b4a4786
Jianguo Ma <jiangma@microsoft.com>
2020-01-19 17:20:51 +0800

    setup filebeat

--- src/main/scripts/setup-filebeat.sh
+++ src/main/scripts/setup-filebeat.sh
@@ -4,7 +4,36 @@ outLogPath=$1 #Log output path
 logStashServerName=$2 #Host name/IP address of LogStash Server
 logStashServerPortNumber=$3 #Port number of LogStash Server
 
-# TODO: Install Filebeat
-echo "Log output path is ${outLogPath}"
-echo "Host name/IP address of LogStash Server is ${logStashServerName}"
-echo "Port number of LogStash Server is ${logStashServerPortNumber}"
+# Install Filebeat
+rpm --import https://artifacts.elastic.co/GPG-KEY-elasticsearch
+cat <<EOF > /etc/yum.repos.d/elastic.repo
+[elasticsearch-7.x]
+name=Elasticsearch repository for 7.x packages
+baseurl=https://artifacts.elastic.co/packages/7.x/yum
+gpgcheck=1
+gpgkey=https://artifacts.elastic.co/GPG-KEY-elasticsearch
+enabled=1
+autorefresh=1
+type=rpm-md
+EOF
+yum install filebeat -y
+
+# Configure Filebeat
+mv /etc/filebeat/filebeat.yml /etc/filebeat/filebeat.yml.bak
+cat <<EOF > /etc/filebeat/filebeat.yml
+filebeat.inputs:
+- type: log
+  paths:
+    - ${outLogPath}
+
+processors:
+- add_cloud_metadata:
+
+output.logstash:
+  hosts: ["${logStashServerName}:${logStashServerPortNumber}"]
+EOF
+
+# Enable & start filebeat
+systemctl daemon-reload
+systemctl enable filebeat
+systemctl start filebeat
